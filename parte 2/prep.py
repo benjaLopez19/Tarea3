1b161d5168c2dfa181d5d91b5cb206acdf7fdf94
benjaLopez19 <82004309+benjaLopez19@users.noreply.github.com>
2022-11-14 22:49:47 -0300

    Update prep.py

--- parte 2/prep.py
+++ parte 2/prep.py
@@ -3,9 +3,17 @@
 import pandas as pd
 import numpy  as np
 
+#cargar archivos dtrn.csv y etrn.csv
+#normalizar
+#reordenar aleatoriamente las posiciones
+#crear etiquetas binarias, vector con clases 1,2 y 3 pasarlo a una matriz Nx3
+# 1 = 1,0,0; 2 = 0,1,0; 3 = 0,0,1 y- 1,2,1,3,2 - [1,0,0],[0,1,0]...
+#crear archivos de datos para training xtrn.csv, ytrn.csv
+#los mismos pasos para testing creando archivos xtst.csv, ytst.csv
+
 # Load data 
 def load_data():
-    ...
+    
     return()
 #Normazationg of the features 
 def norma_data():
